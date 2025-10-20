93ac27eeec94fd2cf803c11ac61a4a51dcb0addf
Taimoor Najam <taimoor_najam@yahoo.com>
2025-10-19 20:47:07 -0400

    tnajam: Set RecyclerView onClick listener via extension function

--- app/src/main/java/com/udacity/project4/locationreminders/reminderslist/ReminderListFragment.kt
+++ app/src/main/java/com/udacity/project4/locationreminders/reminderslist/ReminderListFragment.kt
@@ -15,6 +15,7 @@ import com.udacity.project4.authentication.AuthenticationActivity
 import com.udacity.project4.base.BaseFragment
 import com.udacity.project4.base.NavigationCommand
 import com.udacity.project4.databinding.FragmentRemindersBinding
+import com.udacity.project4.locationreminders.ReminderDescriptionActivity
 import com.udacity.project4.utils.setDisplayHomeAsUpEnabled
 import com.udacity.project4.utils.setTitle
 import com.udacity.project4.utils.setup
@@ -67,8 +68,10 @@ class ReminderListFragment : BaseFragment() {
     }
 
     private fun setupRecyclerView() {
-        val adapter = RemindersListAdapter {}
-        // Setup the recycler view using the extension function
+        val adapter = RemindersListAdapter { reminder ->
+            val intent = ReminderDescriptionActivity.newIntent(requireContext(), reminder)
+            startActivity(intent)
+        }
         binding.reminderssRecyclerView.setup(adapter)
     }
 
