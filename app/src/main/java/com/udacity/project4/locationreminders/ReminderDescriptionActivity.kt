42ab2690caf2192da47efdcd9a0ca21aafbbbcaa
Taimoor Najam <taimoor_najam@yahoo.com>
2025-10-19 20:44:59 -0400

    tnajam: Add layout of reminder description and process intent in that activity

--- app/src/main/java/com/udacity/project4/locationreminders/ReminderDescriptionActivity.kt
+++ app/src/main/java/com/udacity/project4/locationreminders/ReminderDescriptionActivity.kt
@@ -2,7 +2,9 @@ package com.udacity.project4.locationreminders
 
 import android.content.Context
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
+import android.os.Parcelable
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.udacity.project4.R
@@ -17,20 +19,38 @@ class ReminderDescriptionActivity : AppCompatActivity() {
     private lateinit var binding: ActivityReminderDescriptionBinding
 
     companion object {
-        private const val EXTRA_ReminderDataItem = "EXTRA_ReminderDataItem"
+        private const val EXTRA_REMINDER_DATA_ITEM = "EXTRA_REMINDER_DATA_ITEM"
 
         // Receive the reminder object after the user clicks on the notification
         fun newIntent(context: Context, reminderDataItem: ReminderDataItem): Intent {
             val intent = Intent(context, ReminderDescriptionActivity::class.java)
-            intent.putExtra(EXTRA_ReminderDataItem, reminderDataItem)
+            intent.putExtra(EXTRA_REMINDER_DATA_ITEM, reminderDataItem)
             return intent
         }
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val layoutId = R.layout.activity_reminder_description
-        binding = DataBindingUtil.setContentView(this, layoutId)
-        // TODO: Add the implementation of the reminder details
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_reminder_description)
+
+        val reminder = intent.getParcelableExtraCompat<ReminderDataItem>(EXTRA_REMINDER_DATA_ITEM)
+        if (reminder != null) {
+            binding.reminderDataItem = reminder
+        }
+        binding.executePendingBindings()
+    }
+}
+
+/**
+ * This extension function checks the Android version and uses the appropriate
+ * getParcelableExtra method to avoid the NoSuchMethodError on devices
+ * running Android versions older than TIRAMISU (API 33).
+ */
+inline fun <reified T : Parcelable> Intent.getParcelableExtraCompat(key: String): T? {
+    return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+        getParcelableExtra(key, T::class.java)
+    } else {
+        @Suppress("DEPRECATION")
+        getParcelableExtra(key) as? T
     }
 }
--- app/src/main/java/com/udacity/project4/locationreminders/reminderslist/ReminderDataItem.kt
+++ app/src/main/java/com/udacity/project4/locationreminders/reminderslist/ReminderDataItem.kt
@@ -1,11 +1,13 @@
 package com.udacity.project4.locationreminders.reminderslist
 
-import java.io.Serializable
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
 import java.util.*
 
 /**
  * data class acts as a data mapper between the DB and the UI
  */
+@Parcelize
 data class ReminderDataItem(
     var title: String?,
     var description: String?,
@@ -14,4 +16,4 @@ data class ReminderDataItem(
     var longitude: Double?,
     var radius: Double?,
     val id: String = UUID.randomUUID().toString()
-) : Serializable
+) : Parcelable
