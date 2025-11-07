79e194326b42dca9a93a1ab087d17f47ef448d68
Taimoor Najam <taimoor_najam@yahoo.com>
2025-11-06 22:08:36 -0500

    tnajam: Add util classes for tests and implement RemindersListViewModelTest

--- app/src/test/java/com/udacity/project4/locationreminders/data/FakeDataSource.kt
+++ app/src/test/java/com/udacity/project4/locationreminders/data/FakeDataSource.kt
@@ -2,27 +2,48 @@ package com.udacity.project4.locationreminders.data
 
 import com.udacity.project4.locationreminders.data.dto.ReminderDTO
 import com.udacity.project4.locationreminders.data.dto.Result
+import kotlin.collections.find
 
-//Use FakeDataSource that acts as a test double to the LocalDataSource
-class FakeDataSource : ReminderDataSource {
+class FakeDataSource(private var reminders: MutableList<ReminderDTO> = mutableListOf()) : ReminderDataSource {
 
-//    TODO: Create a fake data source to act as a double to the real data source
+    private var shouldReturnError = false
+
+    fun setShouldReturnError(shouldError: Boolean) {
+        this.shouldReturnError = shouldError
+    }
 
     override suspend fun getReminders(): Result<List<ReminderDTO>> {
-        TODO("Return the reminders")
+        if (shouldReturnError) {
+            return Result.Error("Test exception: Error getting reminders")
+        }
+        return Result.Success(ArrayList(reminders))
     }
 
     override suspend fun saveReminder(reminder: ReminderDTO) {
-        TODO("save the reminder")
+        reminders.add(reminder)
     }
 
     override suspend fun getReminder(id: String): Result<ReminderDTO> {
-        TODO("return the reminder with the id")
+        if (shouldReturnError) {
+            return Result.Error("Test exception: Error getting reminder with id $id")
+        }
+        val reminder = reminders.find { it.id == id }
+        return if (reminder != null) {
+            Result.Success(reminder)
+        } else {
+            Result.Error("Reminder not found with id: $id")
+        }
+    }
+
+    override suspend fun deleteReminder(id: String) {
+        val reminder = reminders.find { it.id == id }
+        reminder?.let {
+            reminders.remove(it)
+        }
     }
 
     override suspend fun deleteAllReminders() {
-        TODO("delete all the reminders")
+        reminders.clear()
     }
-
 
 }
